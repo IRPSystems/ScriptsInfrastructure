f281ffcadabb7bc8cac9643369ddd42894fb0801
Smadar Tsdaka <smadar@irpsystems.com>
2024-01-29 14:55:56 +0200

    Changed namespace of RunExplorerView

--- ScriptRunner/Views/RunExplorerView.xaml.cs
+++ ScriptRunner/Views/RunExplorerView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Evva.Views
+namespace ScriptRunner.Views
 {
 	/// <summary>
 	/// Interaction logic for RunExplorerView.xaml
