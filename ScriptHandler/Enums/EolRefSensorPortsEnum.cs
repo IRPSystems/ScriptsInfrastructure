487c8ca9b8b571acd2fef6a7a3d73d3681e5b3be
Smadar Tsdaka <smadar@irpsystems.com>
2024-08-06 15:41:55 +0300

    Add EolRefSensorPortsEnum

---
+++ ScriptHandler/Enums/EolRefSensorPortsEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace ScriptHandler.Enums
+{
+    public enum EolRefSensorPortsEnum
+	{
+        Port1 = 1, 
+        Port2,
+        Port3, 
+        Port4,
+        Port5,
+        Port6,
+        Port7,
+        Port8,
+    }
+}
