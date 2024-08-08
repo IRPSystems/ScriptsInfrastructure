106d6bb85957c5d841e685a2145d64a6b443a1cb
Smadar Tsdaka <smadar@irpsystems.com>
2024-08-08 09:03:16 +0300

    Add EOLStepSummeryData

---
+++ ScriptHandler/Models/EOLStepSummeryData.cs
@@ -0,0 +1,32 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ScriptHandler.Models
+{
+	public class EOLStepSummeryData: ObservableObject
+	{
+		public string StepDescription { get; set; }
+		public string Description { get; set; }
+		public string Value { get; set; }
+		public bool IsPass { get; set; }
+		public string ErrorDescription { get; set; }
+
+		public EOLStepSummeryData() { }
+
+		public EOLStepSummeryData(
+			string stepDescription, 
+			string description, 
+			string value = "", 
+			bool isPass = true, 
+			string errorDescription = "")
+		{
+			StepDescription = stepDescription;
+			Description = description;
+			Value = value;
+			IsPass = isPass;
+			ErrorDescription = errorDescription;
+		}
+	}							  
+}								  
+								  
+								  
