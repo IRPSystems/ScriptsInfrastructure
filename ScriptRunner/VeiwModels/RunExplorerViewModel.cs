3845c043d5ec00d46202e9f269ba7b395ee229f8
Smadar Tsdaka <smadar@irpsystems.com>
2024-11-04 11:39:05 +0200

    Add IsShowButtons to allow to show/hide the to buttons of RunExplorerView

--- ScriptRunner/VeiwModels/RunExplorerViewModel.cs
+++ ScriptRunner/VeiwModels/RunExplorerViewModel.cs
@@ -24,6 +24,8 @@ namespace ScriptRunner.ViewModels
 
 		public GeneratedScriptData SelectedScript { get; set; }
 
+		public bool IsShowButtons { get; set; }
+
 		#endregion Properties
 
 		#region Fields
@@ -52,6 +54,8 @@ namespace ScriptRunner.ViewModels
 			_runScript = runScript;
 			_scriptUserData = scriptUserData;
 
+			IsShowButtons = true;
+
 			ReloadProjectCommand = new RelayCommand<GeneratedProjectData>(ReloadProject);
 			SelectRecordingPathCommand = new RelayCommand<GeneratedProjectData>(SelectRecordingPath);
 			ScriptUpCommand = new RelayCommand<GeneratedProjectData>(ScriptUp);
@@ -70,6 +74,8 @@ namespace ScriptRunner.ViewModels
 			ObservableCollection<GeneratedProjectData> projectsList)
 		{
 			ProjectsList = projectsList;
+
+			IsShowButtons = false;
 		}
 
 		#endregion Constructor
