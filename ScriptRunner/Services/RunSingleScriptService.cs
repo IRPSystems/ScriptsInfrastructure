1b7dba0b4d8b9fe2a881c57527704b7daf2b2f38
Smadar Tsdaka <smadar@irpsystems.com>
2025-03-06 11:08:49 +0200

    Code cosmetics

--- ScriptRunner/Services/RunSingleScriptService.cs
+++ ScriptRunner/Services/RunSingleScriptService.cs
@@ -482,7 +482,7 @@ namespace ScriptRunner.Services
 			OnPropertyChanged(nameof(CurrentStep));
 			
 
-			bool isEnd = Repeat();
+			bool isEnd = RepeatSubScript();
 			if (isEnd)
 			{
 				if (_scriptStep != null)
@@ -500,7 +500,7 @@ namespace ScriptRunner.Services
 			}
 		}
 
-		private bool Repeat()
+		private bool RepeatSubScript()
 		{
 			if (this is RunSingleScriptService_SO)
 			{
