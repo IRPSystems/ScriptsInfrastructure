87227143c27032088adb5ad61deae2f262f14be5
Smadar Tsdaka <smadar@irpsystems.com>
2025-03-06 11:12:26 +0200

    Rename the function Repeat to IsStopRepeatSubScript

--- ScriptRunner/Services/RunSingleScriptService.cs
+++ ScriptRunner/Services/RunSingleScriptService.cs
@@ -482,7 +482,7 @@ namespace ScriptRunner.Services
 			OnPropertyChanged(nameof(CurrentStep));
 			
 
-			bool isEnd = RepeatSubScript();
+			bool isEnd = IsStopRepeatSubScript();
 			if (isEnd)
 			{
 				if (_scriptStep != null)
@@ -500,7 +500,7 @@ namespace ScriptRunner.Services
 			}
 		}
 
-		private bool RepeatSubScript()
+		private bool IsStopRepeatSubScript()
 		{
 			if (this is RunSingleScriptService_SO)
 			{
