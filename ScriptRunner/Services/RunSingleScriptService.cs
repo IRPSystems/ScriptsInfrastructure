6289cf3ee970ce333591e7075ec93135436bf385
Smadar Tsdaka <smadar@irpsystems.com>
2025-03-19 16:33:03 +0200

    Set _subScript to null at end of run

--- ScriptRunner/Services/RunSingleScriptService.cs
+++ ScriptRunner/Services/RunSingleScriptService.cs
@@ -346,6 +346,7 @@ namespace ScriptRunner.Services
 
 					
 					sub.Dispose();
+					_subScript = null;
 				}
 
 				if(_currentStep.CommSendResLog != null)
@@ -580,16 +581,22 @@ namespace ScriptRunner.Services
 
 		private void SubScriptEndedEventHandler(bool isAborted)
 		{
-			_userDecision.Set();
-
-			if(_subScript != null && _subScript.CurrentScript.IsPass == false)
+			try
 			{
-				ScriptErrorMessage += _subScript.ScriptErrorMessage + "\r\n\r\n";
-			}
+				_userDecision.Set();
 
-			//_subScript = null;
-			//StepEnd();
-			OnPropertyChanged(nameof(CurrentStep));
+				if (_subScript != null && _subScript.CurrentScript.IsPass == false)
+				{
+					ScriptErrorMessage += _subScript.ScriptErrorMessage + "\r\n\r\n";
+				}
+
+				//_subScript = null;
+				//StepEnd();
+				OnPropertyChanged(nameof(CurrentStep));
+			}
+			catch 
+			{
+			}
 		}
 
 		#endregion Sub script
