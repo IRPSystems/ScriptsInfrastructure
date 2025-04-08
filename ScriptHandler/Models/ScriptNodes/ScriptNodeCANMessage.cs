61daee937fafc191793762f2a64cdf362c44710c
Smadar Tsdaka <smadar@irpsystems.com>
2025-04-08 15:52:39 +0300

    Allow the user to select the CRC and Counter fields in the DBC message

--- ScriptHandler/Models/ScriptNodes/ScriptNodeCANMessage.cs
+++ ScriptHandler/Models/ScriptNodes/ScriptNodeCANMessage.cs
@@ -102,11 +102,123 @@ namespace ScriptHandler.Models.ScriptNodes
 
 		public bool IsUseCRCandCounter { get; set; }
 
+
+
+
+
 		public bool IsCRCAvailable { get; set; }
-		public string CRCFieldName { get; set; }
+
+		private bool _isSettingCRCField = false;
+		private Signal _crcField;
+		[JsonIgnore]
+		public Signal CRCField
+		{
+			get => _crcField;
+			set
+			{
+				_crcField = value;
+
+				if (_isSettingCRCFieldName == false)
+					return;
+
+				_isSettingCRCField = true;
+				CRCFieldName = value?.Name;
+				_isSettingCRCField = false;
+			}
+		}
+
+		private bool _isSettingCRCFieldName = false;
+		private string _crcFieldName;
+		public string CRCFieldName 
+		{
+			get => _crcFieldName;
+			set
+			{
+				
+				_crcFieldName = value;
+
+				if (_isSettingCRCField)
+					return;
+
+				if (Message == null)
+					return;
+
+				_isSettingCRCFieldName = true;
+
+				foreach (Signal signal in Message.Signals)
+				{
+					if(signal.Name == _crcFieldName)
+					{
+						_crcField = signal;
+						break;
+					}
+				}
+
+				_isSettingCRCFieldName = false;
+			}
+		}
+
+
+
+
+
 
 		public bool IsCounterAvailable { get; set; }
-		public string CounterFieldName { get; set; }
+
+		private bool _isSettingCounterField = false;
+		private Signal _counterField;
+		[JsonIgnore]
+		public Signal CounterField
+		{
+			get => _counterField;
+			set
+			{
+				_counterField = value;
+
+				if (_isSettingCounterFieldName == false)
+					return;
+
+				_isSettingCounterField = true;
+				CounterFieldName = value?.Name;
+				_isSettingCounterField = false;
+			}
+		}
+
+		private bool _isSettingCounterFieldName = false;
+		private string _counterFieldName;
+		public string CounterFieldName
+		{
+			get => _counterFieldName;
+			set
+			{
+
+				_counterFieldName = value;
+
+				if (_isSettingCounterField)
+					return;
+
+				if (Message == null)
+					return;
+
+				_isSettingCounterFieldName = true;
+
+				foreach (Signal signal in Message.Signals)
+				{
+					if (signal.Name == _counterFieldName)
+					{
+						_counterField = signal;
+						break;
+					}
+				}
+
+				_isSettingCounterFieldName = false;
+			}
+		}
+
+
+
+
+
 
 		public int IDInProject { get; set; }
 
