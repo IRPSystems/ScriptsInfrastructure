bf9687aecc793cd250feccd7d4c88e88a69c07ad
Smadar Tsdaka <smadar@irpsystems.com>
2025-05-19 15:16:49 +0300

    Add files used for extra data in Compare

---
+++ ScriptHandler/Converters/DBCVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Globalization;
+using System.Windows.Data;
+using System;
+using System.Windows;
+using DeviceCommunicators.DBC;
+
+namespace ScriptHandler.Converter
+{
+	public class DBCVisibilityConverter : IValueConverter
+	{
+
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is DBC_ParamData)
+				return Visibility.Visible;
+			return Visibility.Collapsed;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
